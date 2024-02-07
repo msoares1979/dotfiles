dbbcc8450be6cbd23aebe0e9918d8970579a4a27
Milton Soares Filho <milton.soares.filho@gmail.com>
2024-02-07 10:53:00 +0100

    custom plugins
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,19 @@
+return {
+  "telescope.nvim",
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+      end,
+      desc = "Find Plugin File",
+    },
+  },
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "c",
+      "cpp",
+      "css",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "regex",
+      "vim",
+      "yaml",
+    },
+  },
+}
