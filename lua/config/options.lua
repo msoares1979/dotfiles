90513b4369d714419701ca4b4291a57eb2c44976
Milton Soares Filho <milton.soares.filho@gmail.com>
2024-02-07 16:57:17 +0100

    sanner split defaults
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,8 @@ vim.wo.number = true
 
 -- hides the tabline, which is the line that displays the tabs at the top of Neovim.
 vim.opt.showtabline = 0
+
+-- open splits in a more natural direction
+-- https://vimtricks.com/p/open-splits-more-naturally/
+vim.opt.splitright = true
+vim.opt.splitbelow = true
