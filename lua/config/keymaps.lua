6eee6dc495812424139b1fddd75521fdd0fa26c4
Milton Soares Filho <milton.soares.filho@gmail.com>
2024-02-29 11:15:33 +0100

    support some of mine personal mappings
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- My Original mappings
+
+keymap.set("n", "<F3>", "<cmd>e %:h<Return>")
+
+keymap.set("n", "<F7>", "<cmd>cp<Return>")
+keymap.set("n", "<F8>", "<cmd>cn<Return>")
+
+-- New mappings
+
+keymap.set("n", "<C-p>", "<cmd>cp<Return>")
+keymap.set("n", "<C-n>", "<cmd>cn<Return>")
+
+-- Window navigation
+keymap.set("n", "<C-Tab>", "gt")
+keymap.set("n", "<C-S-Tab>", "gT")
