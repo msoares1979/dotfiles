ca00289a8924689ab5242b08c207e3009e9dc5d9
Milton Soares Filho <milton.soares.filho@gmail.com>
2024-02-29 11:15:03 +0100

    automatically start inserting in term mode
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+    vim.opt.number = false
+    vim.cmd("startinsert")
+  end,
+})
