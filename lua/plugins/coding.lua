78d3cccbdfd448fb7ad6804319177e68aa568475
Milton Soares Filho <milton.soares.filho@gmail.com>
2024-03-27 11:25:05 +0100

    fixup completion mappings
    Also enabled lua-snippets to avoid completiong error.
    
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -22,30 +22,34 @@ return {
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<S-CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<C-CR>"] = function(fallback)
-            cmp.abort()
-            fallback()
-          end,
+          ["<C-c>"] = cmp.mapping.abort(),
+          --["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          -- ["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true, }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<S-C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true, }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<esc>"] = function(fallback) cmp.abort() fallback() end,
         }),
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        snippet = {
+          expand = function(args)
+            require'luasnip'.lsp_expand(args.body)
+          end
+        },
         sources = cmp.config.sources({
-          -- Copilot Source
-          { name = "copilot", group_index = 2 },
-          -- Other Sources
-          { name = "nvim_lsp", group_index = 2 },
-          { name = "path", group_index = 2 },
-          { name = "luasnip", group_index = 2 },
+          { name = "copilot" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        },
+        {
+          { name = "buffer" },
+          { name = "path" },
         }),
         formatting = {
           format = function(_, item)
@@ -128,4 +132,10 @@ return {
     end
   },
 
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
 }
