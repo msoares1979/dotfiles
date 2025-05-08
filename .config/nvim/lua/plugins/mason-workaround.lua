defd9b7ca3e66c9def7239974f4bf71a2f0bafe9
Milton Soares Filho <milton.soares.filho@gmail.com>
2025-05-08 11:24:39 +0200

    workaround for new mason release incompatibility
    See https://github.com/LazyVim/LazyVim/issues/6039

---
+++ .config/nvim/lua/plugins/mason-workaround.lua
@@ -0,0 +1,6 @@
+-- See https://github.com/LazyVim/LazyVim/issues/6039
+
+return {
+   { "mason-org/mason.nvim", version = "^1.0.0" },
+   { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
