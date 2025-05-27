81bdd5613847de5998184b9d66011779066f0bb9
Milton Soares Filho <milton.soares.filho@gmail.com>
2025-05-27 14:19:44 +0200

    use tempdir for environment setup
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- .local/bin/dotfiles-setup.sh
+++ .local/bin/dotfiles-setup.sh
@@ -2,18 +2,16 @@
 
 bare_repo="${1:-$HOME/.local/share/dotfiles}"
 
+tempdir=$(mktemp -d /tmp/dotfiles-setup.XXXXXX) || exit 1
+
 pushd ${bare_repo}
 
 # temporary directory for the worktree
-git worktree add -b main --lock --reason "this is my HOME directory" $HOME/dotfiles
+git worktree add -b main --lock --reason "this is my HOME directory" "${tempdir}"
 
 # manually move the worktree to the HOME directory
-cp -fv ~/dotfiles/.git ~
-sed -i -e 's@/dotfiles@@' ~/.git
+echo "${base_repo}/worktrees/dotfiles" > ~/.git
 
 echo "${HOME}/.git" > worktree/dotfiles/gitdir
 
 popd
-
-
-
