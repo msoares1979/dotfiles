4e26aceae0eb969e3287fa4b567918a641bc4275
Milton Soares Filho <milton.soares.filho@gmail.com>
2025-06-10 16:06:28 +0200

    nvim: enable cursor animation

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -7,6 +7,21 @@ return {
     }
   },
   {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      smear_between_buffers = true,
+      smear_between_neighbor_lines = true,
+      scroll_buffer_space = true,
+      legacy_computing_symbols_support = true,
+      smear_insert_mode = false,
+      stiffness = 0.8,
+      --trailing_stiffness = 0.5,
+      --stiffness_insert_mode = 0.6,
+      --trailing_stiffness_insert_mode = 0.6,
+      distance_stop_animating = 0.6,
+    },
+  },
+  {
     "snacks.nvim",
     opts = {
       dashboard = {
