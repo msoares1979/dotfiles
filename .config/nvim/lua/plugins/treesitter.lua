104990fde9e75ee301b21ddb57cf75afb8b2bb47
Milton Soares Filho <milton.soares.filho@gmail.com>
2025-06-17 11:16:43 +0200

    use vim.list_extend as auto-merge may not work with lists
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "c",
-      "cmake",
-      "cpp",
-      "css",
-      "html",
-      "gitignore",
-      "graphql",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "regex",
-      "sql",
-      "vim",
-      "yaml",
-    },
-  },
-}
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- auto-merge may not work with lists, so we use vim.list_extend
+      vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "css",
+        "html",
+        "gitignore",
+        "graphql",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "sql",
+        "vim",
+        "yaml",
+      })
+    end,
+  }
