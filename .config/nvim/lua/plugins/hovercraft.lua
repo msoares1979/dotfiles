68c5ccce2cb1bf4756cf76ccba856880904f7d29
Milton Soares Filho <milton.soares.filho@gmail.com>
2025-06-23 10:26:25 +0200

    nvim: hovercraft plugin
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

---
+++ .config/nvim/lua/plugins/hovercraft.lua
@@ -0,0 +1,50 @@
+local M = {
+  'patrickpichler/hovercraft.nvim',
+
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+  },
+
+  debug = true,
+
+  -- this is the default config and can be skipped
+  opts = function()
+    return {
+      providers = {
+        providers = {
+          { 'lsp',  require('hovercraft.provider.lsp.hover').new() },
+          { 'man',  require('hovercraft.provider.man').new() },
+          { 'dict', require('hovercraft.provider.dictionary').new() },
+        }
+      },
+
+      window = { border = 'single', },
+
+      keys = {
+        { '<C-u>', function() require('hovercraft').scroll({ delta = -4 }) end },
+        { '<C-d>', function() require('hovercraft').scroll({ delta = 4 }) end },
+        { '<C-n>', function() require('hovercraft').hover_next() end },
+        { '<C-p>', function() require('hovercraft').hover_next({ step = -1 }) end },
+      }
+    }
+  end,
+
+  event = 'VeryLazy',
+
+  keys = {
+    {
+      "<leader>k",
+      function()
+        local hovercraft = require("hovercraft")
+        if hovercraft.is_visible() then
+          hovercraft.enter_popup()
+        else
+          hovercraft.hover()
+        end
+      end,
+      desc = 'Hovercraft keywords'
+    },
+  },
+}
+
+return M
