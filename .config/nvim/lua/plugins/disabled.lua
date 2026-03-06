576301b03d45a906f12f35e8fb5714e6f6013ad1
Milton Soares Filho <milton.soares.filho@gmail.com>
2026-03-06 11:30:18 +0100

    nvim: mini.pairs source changed on lazyvim

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,5 +2,5 @@ return {
   { "nvimdev/dashboard-nvim", enabled = false },
   { "flash.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
+  { "mini-nvim/mini.pairs", enabled = false },
 }
